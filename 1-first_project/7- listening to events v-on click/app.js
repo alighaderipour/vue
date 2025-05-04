5d04d0f2f79269b90a8c6686f517c3e7bb05fe17
Shahaz <ali.ghaderipour@gmail.com>
2025-05-04 12:48:22 +0330

    v-model :value @input

--- 1-first_project/7- listening to events v-on click/app.js
+++ 1-first_project/7- listening to events v-on click/app.js
@@ -1,19 +1,31 @@
 const vm = Vue.createApp({
-    data(){
-        return{
-                counter : 1
+    data() {
+        return {
+            counter:0,
+            firstName:'ali',
+            lastName : 'ghaderi pour'
 
         }
     },
     // do not use arrow function in methods
-    methods:{
-            increament(){
-                 this.counter++
-},
+    methods: {
+        fullName(){
+           return `${this.firstName} ${this.lastName}`
+        },
+        increament() {
+            this.counter++
+        },
 
-            decreament(){
-                this.counter--
-}
+        decreament() {
+            this.counter--
+        },
+        updateLastName(event){
+            console.log('Full event:', event);
+            console.log('Target value:', event.target.value); // Current input value
+            console.log('Key pressed:', event.data); // The specific character added
+            console.log('Input type:', event.inputType); // What kind of input occurred
+            this.lastName= event.target.value.toUpperCase()
+        }
 
     }
 }).mount('#jasem')
