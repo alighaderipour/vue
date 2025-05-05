87466d9a05b6d3e93afa67e3510ffb69abd85370
Shahaz <ali.ghaderipour@gmail.com>
2025-05-05 13:25:10 +0330

    6

--- 1-first_project/7-3 listening to events value input/app.js
+++ 1-first_project/7-3 listening to events value input/app.js
@@ -1,16 +1,16 @@
 const vm = Vue.createApp({
     data() {
         return {
-            counter:0,
-            firstName:'ali',
-            lastName : 'ghaderi pour'
+            counter: 0,
+            firstName: 'ali',
+            lastName: 'ghaderi pour'
 
         }
     },
     // do not use arrow function in methods
     methods: {
-        fullName(){
-           return `${this.firstName} ${this.lastName}`
+        fullName() {
+            return `${this.firstName} ${this.lastName}`
         },
         increament() {
             this.counter++
@@ -20,16 +20,18 @@ const vm = Vue.createApp({
             this.counter--
         },
 
-        updateFirstName(event){
+        updateFirstName(message,event) {
+            event.preventDefault()
+            console.log(message, event)
+            this.firstName = event.target.value.toUpperCase()
+        },
+        updateLastName(event) {
+
             console.log('Full event:', event);
             console.log('Target value:', event.target.value); // Current input value
             console.log('Key pressed:', event.data); // The specific character added
             console.log('Input type:', event.inputType); // What kind of input occurred
-            this.firstName = event.target.value.toUpperCase()
-        },
-         updateLastName(event){
-
-            this.lastName= event.target.value.toUpperCase()
+            this.lastName = event.target.value.toUpperCase()
         },
 
     }
