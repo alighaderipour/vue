89f4a7a28abd73dc86217c71b7b80bb635993d3c
Shahaz <ali.ghaderipour@gmail.com>
2025-05-15 05:54:59 +0330

    callback functions

---
+++ 6-/3-validation_props/ValidationProps/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import User from './components/User.vue'
+export default {
+    name: 'app',
+    components:{
+        User
+    },
+    data() {
+        return {
+            age:32,
+            user_name:222,
+            job:"employed"
+        }
+    },
+    methods:{
+        increaseAge(num){
+            return this.age+=num
+        },
+        decreaseAge(num){
+            return this.age-= num
+        }
+    }
+
+}
+</script>
+
+<template>
+  <div>
+    <User :age="age" @increase-age="increaseAge" :decrease-age="decreaseAge"></User>
+  </div>
+</template>
+
+<style></style>
---
+++ 6-/3-validation_props/ValidationProps/src/components/User.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: "user",
+  props: {
+    age: {
+      type: Number,
+      validator(value) {
+        return value < 130;
+      },
+    },
+    user_name: {
+      type: Number,
+      required: true,
+    },
+    job: {
+      type: String,
+      default: "employed",
+    },
+    decreaseAge:Function
+
+  },
+  data() {
+    return {};
+  },
+  methods:{
+    increaseAge(num){
+        this.$emit("increase-age", num)
+    }
+  }
+};
+</script>
+
+<template>
+    <button type="button" @click="increaseAge(1)"> increase age </button>
+    <button type="button" @click="decreaseAge(3)"> decrease age </button>
+  <div>you are {{ age }}</div>
+</template>
+
+<style></style>
