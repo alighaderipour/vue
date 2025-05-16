a717cc71e2898c6c42c2b485702eb3758ef0e3ce
Shahaz <ali.ghaderipour@gmail.com>
2025-05-16 10:58:53 +0330

    7-1

--- 7-animations/1-cc_transition/css-transitions/src/App.vue
+++ 7-animations/1-cc_transition/css-transitions/src/App.vue
@@ -1,19 +1,43 @@
 <script>
 export default {
-    name: 'App',
-    data() {
-        return {
-          flag:false
-        }
+  name: "App",
+  data() {
+    return {
+      flag: true,
+    };
+  },
+  methods:{
+    flagToggle(){
+        return this.flag = !this.flag
     }
-}
+  }
+};
 </script>
 
 <template>
+  <div>
+    <button type="button" @click="flagToggle">{{ flag ? 'bye' : 'hello' }}</button>
     <div>
-       <h1></h1>
+      <Transition name="fade" duration="1000">
+        
+        <h1 v-if="flag">hello</h1>
+        <h1 v-else>bye</h1>
+      </Transition>
+     
+
     </div>
+  </div>
 </template>
 
 <style>
+.fade-enter-from{
+opacity: 0;
+}
+.fade-enter-active{
+transition: all 1.25s linear;
+}
+.fade-leave-to{
+  transition: all 1.05s linear;
+  opacity: 0;
+}
 </style>
