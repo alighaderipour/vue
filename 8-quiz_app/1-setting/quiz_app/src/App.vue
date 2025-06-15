74db813a8d0773cc7fb7e482b79be08effe05892
Shahaz <ali.ghaderipour@gmail.com>
2025-06-15 13:02:38 +0330

    test

--- 8-quiz_app/1-setting/quiz_app/src/App.vue
+++ 8-quiz_app/1-setting/quiz_app/src/App.vue
@@ -1,14 +1,38 @@
+<template>
+  <div class="ctr">
+    <div class="questions-ctr">
+        <div class="progress">
+            <div class="bar"></div>
+            <div class="status">1 out of 3 questions answered</div>
+        </div>
+        <div class="single-question">
+            <div class="question">Sample Question 1</div>
+            <div class="answers">
+                <div class="answer">Sample Answer 1</div>
+                <div class="answer">Sample Answer 2</div>
+                <div class="answer">Sample Answer 3</div>
+                <div class="answer">Sample Answer 4</div>
+            </div>
+        </div>
+    </div>
+    <div class="result">
+        <div class="title">You got sample result 1!</div>
+        <div class="desc">
+            Enter a short description here about the result.
+        </div>
+    </div>
+    <button type="button" class="reset-btn">Reset</button>
+</div>
+</template>
+
+
 <script>
-import Questions from './components/Questions.vue';
-import Result from './components/Result.vue';
 export default {
-    name: 'App',
-    data() {
-       return{
-        questionAnswered:0,
-    questions: [
+  data(){
+    return {
+        questions: [
         {
-            q: 'What is 2 + 2?', 
+            q: 'What is 2 + 2?',
             answers: [
                 {
                     text: '4',
@@ -16,20 +40,20 @@ export default {
                 },
                 {
                     text: '3',
-                    is_correct: false 
+                    is_correct: false
                 },
                 {
                     text: 'Fish',
-                    is_correct: false 
+                    is_correct: false
                 },
                 {
                     text: '5',
-                    is_correct: false 
+                    is_correct: false
                 }
-            ] 
+            ]
         },
-        { 
-            q: 'How many letters are in the word "Banana"?', 
+        {
+            q: 'How many letters are in the word "Banana"?',
             answers: [
                 {
                     text: '5',
@@ -37,20 +61,20 @@ export default {
                 },
                 {
                     text: '7',
-                    is_correct: false 
+                    is_correct: false
                 },
                 {
                     text: '6',
-                    is_correct: true 
+                    is_correct: true
                 },
                 {
                     text: '12',
-                    is_correct: false 
+                    is_correct: false
                 }
-            ] 
+            ]
         },
-        { 
-            q: 'Find the missing letter: C_ke', 
+        {
+            q: 'Find the missing letter: C_ke',
             answers: [
                 {
                     text: 'e',
@@ -58,14 +82,13 @@ export default {
                 },
                 {
                     text: 'a',
-                    is_correct: true 
+                    is_correct: true
                 },
                 {
                     text: 'i',
-                    is_correct: false 
+                    is_correct: false
                 }
-                
-            ] 
+            ]
         },
     ],
     results: [
@@ -82,25 +105,13 @@ export default {
             desc: "Studying has definitely paid off for you!"
         }
     ]
-}
-    },
-    components:{
-      Questions,
-      Result
+
     }
+  }
 }
 </script>
 
-<template>
-   <div class="ctr">
-   <questions :questions="questions" v-if="questionAnswered < questions.length"/>
-   <result v-else/>
-    
-    <button type="button" class="reset-btn">Reset</button>
-</div>
-</template>
-
-<style>
+<style scoped>
 * {
   box-sizing: border-box;
 }
@@ -210,7 +221,7 @@ body {
 }
 .fade-leave-to {
   opacity: 0;
-} 
+}
 
 .reset-btn {
   background-color: #ff6372;
@@ -222,7 +233,7 @@ body {
     display: block;
 }
 
-.result{ 
+.result{
   width: 100%;
 }
 
