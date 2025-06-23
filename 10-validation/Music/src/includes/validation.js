f2a9e7e3d6031a9f80d6f47cdae1929fedaa3fb8
Shahaz <ali.ghaderipour@gmail.com>
2025-06-23 12:03:09 +0330

    errormessage

--- 10-validation/Music/src/includes/validation.js
+++ 10-validation/Music/src/includes/validation.js
@@ -1,7 +1,11 @@
-import {Form as VeeForm, Field as VeeField} from "vee-validate"
-export default{
-    install(app){
-        app.component('VeeForm', VeeForm),
-        app.component('VeeField', VeeField)
-    },
-};
+// this is where we define our custom plugins
+import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from 'vee-validate'
+import { required } from '@vee-validate/rules'
+export default {
+  install(app) {
+    app.component('VeeForm', VeeForm)
+    app.component('VeeField', VeeField)
+    app.component('ErrorMessage', ErrorMessage)
+    defineRule('required', required)
+  },
+}
