5a810937c5a0519d6f2ce5db287746028a228e44
Shahaz <ali.ghaderipour@gmail.com>
2025-06-23 13:10:04 +0330

    lat

--- 10-validation/Music/src/components/Auth.vue
+++ 10-validation/Music/src/components/Auth.vue
@@ -86,11 +86,12 @@
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
               <vee-field
-                type="text" name="name"
+                type="text"
+                name="name"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Name"/>
-                <ErrorMessage class="text-red-600" name="name"/>
-              
+                placeholder="Enter Name"
+              />
+              <ErrorMessage class="text-red-600" name="name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
